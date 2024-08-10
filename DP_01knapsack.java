4c606d2ea3abd102ca7c1df576ec2405c2a3b66b
Mohd Aman <159934544+getwithaman@users.noreply.github.com>
2024-08-11 00:03:26 +0530

    Add files via upload

---
+++ DP_01knapsack.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class DP_01knapsack {
+    // recursion
+    // public static int knapsack(int val[],int wt[],int w,int n){
+    // if(w==0||n==0){
+    // return 0;
+    // }
+    // if(wt[n-1]<=w){
+    // //include
+    // int ans1 = val[n-1]+knapsack(val, wt, w-wt[n-1], n-1);
+    // //exclude
+    // int ans2 = knapsack(val, wt, w, n-1);
+    // return Math.max(ans1, ans2);
+    // } 
+    // else{
+    // return knapsack(val, wt, w, n-1);
+    // }
+    // }
+    // memoization
+    public static int knapsack(int val[],int wt[],int w,int n,int dp[][]){
+        if(w==0||n==0){
+            return 0;
+        }
+        
+        if(dp[n][w]!=-1){
+            return dp[n][w];
+        }
+
+        if(wt[n-1]<=w){
+            //include
+            int ans1 = val[n-1]+knapsack(val, wt, w-wt[n-1], n-1,dp);
+            //exclude
+            int ans2 = knapsack(val, wt, w, n-1,dp);
+            dp[n][w] = Math.max(ans1, ans2);
+            return dp[n][w];
+        }
+        else{
+            dp[n][w] = knapsack(val, wt, w, n-1,dp);
+            return dp[n][w];
+        }
+    }
+    public static void main(String[] args) {
+        //recursion
+        // int val[]={15,14,10,45,30};
+        // int wt[]={2,5,1,3,4};
+        // int w = 7;
+        // System.out.println(knapsack(val, wt, w, val.length));
+
+        //memoization
+        int val[]={6,4,5,3,9,7}; 
+        int wt[]={4,2,3,1,6,4};
+        int w = 10;
+        int dp[][] = new int[val.length+1][w+1];
+        for(int i =0;i<dp.length;i++){
+            for(int j = 0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
+        }
+        System.out.println(knapsack(val, wt, w, val.length,dp));
+    }
+}
+
